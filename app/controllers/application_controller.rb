ae49f2e71295a241b45eb79be9027bfe6934c56f
programaker <programaker@gmail.com>
2011-06-17 16:51:04 -0300

    colocando o setup do facebook no ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :setup
+private
+  def setup
+    rest_graph_setup(
+      :app_id => '219508714739118',
+      :canvas => 'Pocbuque',
+      :auto_authorize_scope => 'email',
+      :secret => '00355ed7f4190c450e4b0d8e5a700084'
+    )
+  end
 end
--- app/controllers/fb_query_controller_controller.rb
+++ app/controllers/fb_query_controller_controller.rb
@@ -1,20 +1,10 @@
 class FbQueryControllerController < ApplicationController
   include RestGraph::RailsUtil
-  before_filter :setup
 
   def index
     if params[:query]
       query = CGI.unescape params[:query]
       @result = rest_graph.fql query
     end
-  end
-private
-  def setup
-    rest_graph_setup(
-      :app_id => '219508714739118',
-      :canvas => 'Pocbuque',
-      :auto_authorize_scope => 'email',
-      :secret => '00355ed7f4190c450e4b0d8e5a700084'
-    )
   end
 end
