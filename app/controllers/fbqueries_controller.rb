e371ade172a6a0d3a0af301670e6e4bcdc6ff953
programaker <programaker@gmail.com>
2011-06-17 23:31:00 -0300

    ufa, corrigido!

--- app/controllers/fbqueries_controller.rb
+++ app/controllers/fbqueries_controller.rb
@@ -1,9 +1,16 @@
 class FbqueriesController < ApplicationController
   def index
+    # esta requisição é apenas para gerar aquele redirect de autorização
+    # ele, por sua vez, vai gerar um POST com a mesma url do form, com os mesmos parametros
+    # esse POST vai cair na action create e aí sim vai exibir os resultados na página!
+    # a requisição só pode ser feita se o parametro estiver presente, pra não estragar o 'back' na pagina de resultados
+    rest_graph.get('me') if params[:query]
   end
 
   def create
-    @query = CGI.unescape params[:query]
-    @result = rest_graph.fql @query
+    if params[:query]
+      @query = CGI.unescape params[:query]
+      @result = rest_graph.fql @query
+    end
   end
 end
